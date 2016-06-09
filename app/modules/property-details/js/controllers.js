ee882b136f9a94f3bf7290fc92c9697057c93dd5
Renato Rodrigues <renato.sp@gmail.com>
2016-06-09 03:26:46 -0300

    Property details screen (\o/)

--- app/modules/property-details/js/controllers.js
+++ app/modules/property-details/js/controllers.js
@@ -2,28 +2,22 @@
 
 angular.module('spotippos.propertyDetails.controllers',[])
     .controller('propertyDetailsController',['$rootScope', '$scope', '$state', '$stateParams', '$filter', 'propertyDetailsService', 'SPOTIPPOS_BOUNDS',
-		function($rootScope, $scope, $state, $stateParams, $filter, propertyDetailsService, SPOTIPPOS_BOUNDS) {
-		
-		$scope.goBack = function() {
-			$state.go("results");
-		};
-		
+        function($rootScope, $scope, $state, $stateParams, $filter, propertyDetailsService, SPOTIPPOS_BOUNDS) {
+        
         $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
             if(error.noPropertyId) {
                 $state.go("results");
             }
         });
-		
+        
         (function () {
             propertyDetailsService.getPropertyById($stateParams.id).then(function(data){
                 $scope.property = data;
-                
-                var map = { width: 298, height: 213 }; //todo
-                
-                var proportion = SPOTIPPOS_BOUNDS.by / map.width;
-                
-                $scope.posX = Math.ceil($scope.property.lat / proportion) + 'px';
-                $scope.posY = Math.ceil($scope.property.long / proportion) + 'px';
+                var posX = parseFloat(($scope.property.lat / SPOTIPPOS_BOUNDS.by) * 100).toFixed(2),
+                    posY = parseFloat(($scope.property.long / SPOTIPPOS_BOUNDS.bx) * 100).toFixed(2);
+                    
+                $scope.posX = posX + '%';
+                $scope.posY = posY + '%';
             });
         }());
         
