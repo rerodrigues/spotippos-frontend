adf658dc9c42ec51b0f2ff7cc5154dc0fa9fa3d2
Renato Rodrigues <renato.sp@gmail.com>
2016-07-19 18:51:20 -0300

    Map directive enhancements

--- app/modules/property-details/js/directives.js
+++ app/modules/property-details/js/directives.js
@@ -5,12 +5,10 @@ angular.module('spotippos.propertyDetails.directives',[])
         return {
             restrict: 'AE',
             replace: true,
-            scope : {},
-            templateUrl: 'modules/property-details/views/_map.html',
-            link : function(scope, elem, attrs){
-                var propertyLocation = attrs['propertyLocation'] && JSON.parse(attrs['propertyLocation']);
-                if(propertyLocation) { scope.location = propertyLocation; }
-            }
+            scope : {
+                location : '=propertyLocation'
+            },
+            templateUrl: 'modules/property-details/views/_map.html'
         };
     })
     .directive('propertyNavigation', function() {
